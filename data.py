b934236f3d10d9977f5ed065be9069da83ea5fd8
Tarrun <52660661+tarrun99@users.noreply.github.com>
2021-04-06 12:56:47 +0530

    data and main files

---
+++ data.py
@@ -0,0 +1,5 @@
+lst = [
+    ["https://gitam.zoom.us/j/94523590116?pwd=a0duazVDbXBKSDlyRFdURVhWWGJVUT09", "14:00", "14:50"]  # Enter your meeting links and duration of entering into the meeting and exiting
+     ["https://gitam.zoom.us/j/97612188240?pwd=QTVSVk1icWZSTjBTVDI4NFJwT1psUT09", "15:00", "16:00"],
+    # ["link3", "3:01", "3:30"]
+]
